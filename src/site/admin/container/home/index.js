28a2964a1bbd9fe92ba8ad83433f82456baf405d
Trinh Cong Dat <dat.tc@isofhcare.com>
2020-08-19 16:14:37 +0700

    add antd design to project

--- src/site/admin/container/home/index.js
+++ src/site/admin/container/home/index.js
@@ -1,26 +1,5 @@
 import React, { Component } from 'react';
 
-
-// const Green = (props) => (
-//   <div className="green">{props.number}</div>
-// )
-// const Blue = (props) => (
-//   <div className="blue">
-//     <Green number={props.number} />
-//   </div>
-// )
-// class Red extends Component {
-//   state = {
-//     number: 10
-//   }
-//   render() {
-//     return <div className="red">
-
-//       <Blue number={this.state.number} />
-//     </div>
-//   }
-// }
-
 const AppContext = React.createContext()
 class AppProvider extends Component {
   state = {
